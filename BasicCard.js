35d65b5c90260467cc2c62ce10150bb2ff402ae4
Will H <wj_hilliard@yahoo.com>
2017-08-05 02:19:46 -0400

    basic card

---
+++ BasicCard.js
@@ -0,0 +1,20 @@
+// **Basic** flashcards, which have a front (_"Who was the first president of the United States?"_),
+// and a back (_"George Washington"_).
+var BasicCard = function(front, back){
+	this.front = front;
+	this.back = back;
+	this.printStats = function(){
+		console.log("")
+	}
+}
+
+var basic1 = new BasicCard("What is the twisted ladder shape of a DNA molecule called?", "A double helix");
+var basic2 = new BasicCard("What is a man-made molecule that can be used to alter DNA?", "CRISPR");
+console.log(basic1);
+console.log();
+console.log(basic2);
+//1 - a `front` property that contains the text on the front of the card.
+
+//2 - constructed object should have a `back` property
+
+//module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,17 @@
+//2. **Cloze-Deleted** flashcards, which present _partial_ text 
+//(_"... was the first president of the United States."_),
+// and the full text when the user requests it (_"George Washington was the first president of the United States."_)
+//This file should define a Node module that exports a constructor 
+//for creating cloze-deletion flashcards, e.g.:`module.exports = ClozeCard;`
+
+//* The constructor should accept two arguments: `text` and `cloze`.
+//The constructor will have 3 properties
+//`cloze` property that contains _only_ the cloze-deleted portion of the text.
+
+//`partial` property that contains _only_ the partial text.
+
+//`fullText` property that contains _only_ the full text.
+
+//throw or log an error when the cloze deletion does _not_ appear in the input text.
+
+//Use prototypes to attach these methods, wherever possible.
