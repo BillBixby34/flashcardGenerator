5de407e832aa02176dcd84e82b8f62343f2ba613
Will H <wj_hilliard@yahoo.com>
2017-08-08 14:43:32 -0400

    added some clozecard functionality

--- CLI.js
+++ CLI.js
@@ -1,20 +1,8 @@
 var inquirer = require("inquirer");
-var BasicCard = require("./BasicCard");
+var ClozeCard = require("./ClozeCard");
 
-var createCard = function() {
-  
-    inquirer.prompt([
-      {
-        name: "cardType",
-        message: "Do you want a basic or cloze card? Please type 'basic' or 'cloze'",
-      }
-      ]).then(function(answers) {
-      	if (answers.cardType === 'basic'){
+//var count = 0;
+//var cardArray = [];
 
-      		var basic1 = new BasicCard("What is the twisted ladder shape of a DNA molecule called?", "A double helix");
-			var basic2 = new BasicCard("What is a man-made molecule that can be used to alter DNA?", "CRISPR");
-			basic1.showFront();
-      	}
-      	});
-  };
-  createCard();
+var card1 = new BasicCard("front side", "back side");
+console.log(card1.front, card1.back);
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,9 +1,15 @@
+var BasicCard = require("./BasicCard");
 //2. **Cloze-Deleted** flashcards, which present _partial_ text 
 //(_"... was the first president of the United States."_),
 // and the full text when the user requests it (_"George Washington was the first president of the United States."_)
 //This file should define a Node module that exports a constructor 
 //for creating cloze-deletion flashcards, e.g.:`module.exports = ClozeCard;`
-
+function ClozeCard(text, cloze){
+	this.text = text;//full text
+	this.cloze = cloze;//instance of text in this.text
+	}
+	ClozeCard.prototype.partial = function(){
+		this.partial = (this.text).replace(this.cloze,"______")
 //* The constructor should accept two arguments: `text` and `cloze`.
 //The constructor will have 3 properties
 //`cloze` property that contains _only_ the cloze-deleted portion of the text.
@@ -13,5 +19,6 @@
 //`fullText` property that contains _only_ the full text.
 
 //throw or log an error when the cloze deletion does _not_ appear in the input text.
-
+//inquirer validate(function){}
+module.exports = ClozeCard;
 //Use prototypes to attach these methods, wherever possible.
